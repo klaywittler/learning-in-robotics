pendulum fixes and implementing policy gradient

project5/lakeTest.py
@@ -5,20 +5,21 @@ from gym import spaces
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.autograd import Variable
 import lake_env
 from tester import Tester
 
-class Policy(nn.Module):
+class policy(nn.Module):
     def __init__(self):
-        super(Policy, self).__init__()
-        self.state_space = env.observation_space.shape[0]
+        super(policy, self).__init__()
+        self.state_space = env.observation_space.n
         self.action_space = env.action_space.n
         self.l1 = nn.Linear(self.state_space, 128, bias=False)
         self.l2 = nn.Linear(128,self.action_space, bias=False)
 
-        self.gamma = gamma
+        self.gamma = options['gamma']
 
+        self.gamma = 0.9
         # Episode policy and reward history
         self.policy_history = Variable(torch.Tensor())
         self.reward_episode = []
@@ -35,80 +36,124 @@ class Policy(nn.Module):
         return model(x)
 
 
-def select_Action(state)
+def main(episodes=4,steps=100):
+    running_reward = 1
+    for episode in range(episodes):
+        state = env.reset()
+        done = False
+        for time in range(steps):
+            action = select_action(state)
+
+            state, reward, done, _ = env.step(action.data[0])
+            policy.reward_episode.append(reward)
+            if done: 
+                break
+
+        running_reward = (running_reward*0.99) + (time*0.01)
+
+        update_policy()
+
+        if episode % 50 == 0:
+            print('Episode {}\tLst length: {:5d}\tAverage length: {:0.2f}'.format(episode, time, running_reward))
+        if running_reward > end.spec.reward_threshold:
+            print('Solved! Running reward is now {} and the last episode runs to {} time steps'.format(running_reward, time))
+            break
+
+def select_action(state):
+    state = torch.from_numpy(state).type(torch.FloatTensor)
+    state = policy(Variable(state))
+    c = Categorical(state)
+    actoin = c.sample()
+
+    if policy.policy_history.dim() != 0:
+        policy.policy_history = torch.cat([policy.policy_gistory, c.log_prob(action)])
+    else:
+        policy.policy_history = (c.log_prob(action))
+    return action
+
+def update_policy():
+    R = 0
+    rewards = []
+
+    # Discount future rewards back to present using gamma
+    for r in policy.reward_episode[::-1]:
+        R = r + policy.gamma * R
+        rewards.inter(0,R)
+
+    # Scale rewards    
+    rewards = torch.FloatTensor(rewards)
+    rewards = (rewards - rewards.mean())/(rewards.std() + np.finfo(npfloat32).eps)
+
+    # Calculate loss 
+    loss = (torch.sum(torch.mul(policy.policy_history, Variable(rewards)).mul(-1), -1))
+
+    # Update network weights
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # SAve and intialize episode history counters
+    policy.loss_history.append(loss.data[0])
+    policy.reward_history.append(np.sum(policy.reward_episode))
+    policy.policy_history = Variable(torch.Tensor())
+    policy.reward_episode = []
 
 
-def policy_improvement(env,gamma,policy,v):
-  P = np.zeros([env.nS,env.nS,env.nA])
-  R = np.zeros([env.nS,env.nS,env.nA])
-  for action in range(env.nA):
-    for state in range(env.nS):
-      for (prob, nextstate, r, is_terminal) in env.P[state][action]:
-        P[state,nextstate,action] += prob  
-        R[state,nextstate,action] += r
-  
-  q = np.sum(np.multiply(P,R + gamma*np.repeat(v[:,np.newaxis],env.nA,axis=1)),axis=1)
-  policy = np.argmax(q,axis=1)
-  return policy
+def getData(episodes=10,steps=1000):
+    inputs = np.zeros([episodes*steps,5])
+    labels = np.zeros([episodes*steps,4])
 
-
-# def get_trainingData(episodes=10,goal_steps=1000):
-#     training_data = np.empty(9)
-
-#     for i_episode in range(episodes):
-#         observation = env.reset()
-#         print(observation)
-#         for t in range(goal_steps):
-#             # env.render()
-#             action = env.action_space.sample()
-#             print(action)
-#             observation, reward, done, info = env.step(action)
+    for episode in range(episodes):
+        observation = env.reset()
+        # state vector:[ cos(theta)   ,  sin(theta)   ,  theta                , theta dot     ]
+        s0 = np.array([observation[0], observation[1], m.acos(observation[0]), observation[2]])
+        for t in range(steps):
+            # env.render()
+            action = env.action_space.sample()
+            inputs[(episode*steps)+t,:] = np.hstack((s0,action))
+            observation, reward, done, info = env.step(action)
             
-#             if done:
-#                 break
-                
-#     env.close()
-#     # training_data = np.delete(training_data,0,axis=0)
-    # return 0
+            s1 = np.array([observation[0], observation[1], m.acos(observation[0]), observation[2]])
+            labels[(episode*steps)+t,:] = s1
+            s0 = s1
 
-# def training():
-
+    env.close()
+    training_data = {'inputs':inputs, 'labels':labels}
+    return training_data
 
 
+if __name__ == '__main__':
+    # # P : s a probability reward, terminal
+    ###################
+    # Policy Gradient #
+    ###################
+    gamma = 0.9
+    episodes = 10
+    steps = 10
+    # env = gym.make('Stochastic-4x4-FrozenLake-v0')
+    env = gym.make('CartPole-v0')
+    env.reset()
+    env.render()
+    main()
+    # torch.save(model,'model.pt')
+     
+    # https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#sphx-glr-beginner-blitz-neural-networks-tutorial-py
+    
 
-#   torch.save(model,'model.pt')
 
-episodes = 10
-goal_steps = 10
-# P : s a probability reward, terminal
-env = gym.make('Stochastic-4x4-FrozenLake-v0')
-env.render()
-for i_episode in range(episodes):
-      observation = env.reset()
-      # print(observation)
-      for t in range(goal_steps):
-          # env.render()
-          action = env.action_space.sample()
-          print(action)
-          observation, reward, done, info = env.step(action)
-          
-          if done:
-              break
-              
-# net = Net()
-# https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#sphx-glr-beginner-blitz-neural-networks-tutorial-py
-gamma = 0.9
+    ##############################
+    # Policy Iteration/Value #
+    ##############################
+    # tester = Tester()
 
-tester = Tester()
+    # [v, i] = tester.value_iteration(env, gamma)
+    # policy = policy = np.random.randint(env.nA, size= env.nS)
+    # policy = tester.policy_selection(env,gamma,policy,v)
+    # print(v,i)
+    # print(policy)
 
-[v, i] = tester.value_iteration(env, gamma)
-policy = policy = np.random.randint(env.nA, size= env.nS)
-policy = policy_improvement(env,gamma,policy,v)
-print(v,i)
-print(policy)
+    # policy, v, c, nv = tester.policy_iteration(env, gamma)
+    # print(v, c, nv)
+    # print(policy)
 
-policy, v, c, nv = tester.policy_iteration(env, gamma)
-print(v, c, nv)
-print(policy)
-
-env.close()
+    # env.close()

project5/pendulum.py
@@ -4,6 +4,8 @@ import gym
 from gym import spaces
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from time import time
 
 class Model(nn.Module):
     def __init__(self):
@@ -12,28 +14,37 @@ class Model(nn.Module):
         # self.action_space = env.action_space.shape[0]
         self.state_space = 4
         self.action_space = 1
-        self.l1 = nn.RNN(self.state_space + self.action_space, 128)
-        self.l2 = nn.RNN(128,self.self.state_space)
+        self.l1 = nn.Linear(5, 128)
+        self.l2 = nn.Linear(128,4)
     def forward(self, x):
+        # x = F.relu(self.l1(x))
+        # x = self.l2(x)
+        # return x
         model = nn.Sequential(
             self.l1,
             nn.Dropout(p=0.6),
-            nn.ReLu(),
+            nn.ReLU(),
             self.l2,
-            nn.ReLu())
-
+            nn.ReLU())
         return model(x)
 
 
 class Trainer(object):
     def __init__(self, options):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = Model()
+        self.options = options
+        # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cpu')
+        self.model = Model().to(self.device)
         # define loss function and optimizer
         self.loss = torch.nn.MSELoss().to(self.device) # for Global loss
         self.optimizer = torch.optim.Adam(self.model.parameters(),lr = options['lr'])
-        self.train_inputs = options['training_data']['training_inputs']
-        self.train_labels = options['training_data']['training_labels']
+        self.train_inputs = torch.from_numpy(options['training_data']['inputs']).float()
+        self.train_labels = torch.from_numpy(options['training_data']['labels']).float()
+        self.test_inputs = torch.from_numpy(options['testing_data']['inputs']).float()
+        self.test_labels = torch.from_numpy(options['testing_data']['labels']).float()
+
+        self.train_loss = []
+        self.test_loss = []
 
     def train(self):
         self.total_step_count = 0
@@ -44,7 +55,7 @@ class Trainer(object):
             remaining_time = (self.options['num_epochs'] - epoch) * seconds_per_epoch
 
             print("Epoch %d/%d - Elapsed Time %f - Remaining Time %f" %
-                    (epoch, self.options.num_epochs,
+                    (epoch, self.options['num_epochs'],
                         elapsed_time, remaining_time))
 
             for step, inputs in enumerate(self.train_inputs):
@@ -52,32 +63,100 @@ class Trainer(object):
                 self.optimizer.zero_grad()
             
                 pred = self.model(inputs)
-                loss = self.loss(pred[-1], self.training_labels[step,:])
+                loss = self.loss(pred[-1], self.train_labels[step,:])
                 loss.backward()
                 self.optimizer.step()
 
+    def test(self):
+        for step, inputs in enumerate(self.test_inputs):             
+            self.model.eval()
+            with torch.no_grad():
+                pred = self.model(inputs)
+                loss = self.loss(pred[-1], self.train_labels[step,:])
 
-def get_trainingData(episodes=10,goal_steps=1000):
-    training_inputs = np.zeros([episodes*goal_steps,5])
-    training_labels = np.zeros([episodes*goal_steps,4])
+        return loss
 
-    for i_episode in range(episodes):
+
+def getData(episodes=10,steps=1000):
+    inputs = np.zeros([episodes*steps,5])
+    labels = np.zeros([episodes*steps,4])
+
+    for episode in range(episodes):
         observation = env.reset()
         # state vector:[ cos(theta)   ,  sin(theta)   ,  theta                , theta dot     ]
         s0 = np.array([observation[0], observation[1], m.acos(observation[0]), observation[2]])
-        for t in range(goal_steps):
+        for t in range(steps):
             # env.render()
             action = env.action_space.sample()
-            training_inputs[(i_episode*goal_steps)+t,:] = np.hstack((s0,action))
+            inputs[(episode*steps)+t,:] = np.hstack((s0,action))
             observation, reward, done, info = env.step(action)
             
             s1 = np.array([observation[0], observation[1], m.acos(observation[0]), observation[2]])
-            training_labels[(i_episode*goal_steps)+t,:] = s1
+            labels[(episode*steps)+t,:] = s1
             s0 = s1
 
     env.close()
-    training_data = {'training_inputs':training_inputs, 'training_labels':training_labels}
+    training_data = {'inputs':inputs, 'labels':labels}
     return training_data
+
+
+def Astar(model, n_theta = 35, n_dtheta = 35, n_u = 35):
+    theta_lower = -m.pi
+    theta_upper = m.pi
+    dtheta_lower = -8
+    dtheta_upper = 8
+    u_lower = -2
+    u_upper = 2
+
+    theta_space = np.linspace(theta_lower,theta_upper,n_theta)
+    dtheta_space = np.linspace(dtheta_lower,dtheta_upper,n_dtheta)
+    action_space = np.linspace(u_lower,u_upper,n_u)
+
+    cost = np.empty([theta_space.size,dtheta_space.size,action_space.size])
+
+
+    t=0
+    for theta in theta_space:
+        dt = 0
+        for dtheta in dtheta_space:
+            a = 0
+            for action in action_space:
+                sa0 = np.array([m.cos(theta), m.sin(theta), theta, dtheta, action])
+                predict_state = model.predict(sa0.reshape(-1,len(sa0)))
+                # heuristic
+                h = -(predict_state[0][2]**2 + predict_state[0][3]**2 + 0.001*action**2)
+                g = -abs(predict_state[0][2]) #-abs(predict_state[0][3])
+                cost[t,dt,a] = h + g
+                a += 1
+            dt += 1
+        t += 1  
+
+    return [cost, theta_space, dtheta_space, action_space]
+
+def simulation(model,games=1,steps=1000):
+    [cost, theta_space, dtheta_space, action_space] = get_costMap(model)
+    for i_episode in range(games):
+        observation = env.reset()
+        for t in range(steps):
+            env.render()
+            theta = m.acos(observation[0]) 
+            dtheta = observation[2]
+            t = np.where(theta_space>=theta) 
+            dt = np.where(dtheta_space>=dtheta)
+            a = Astar(model,theta,dtheta)
+            a = cost[t[0][0],dt[0][0],:].argmax()
+            action = [action_space[a]]
+            # if abs(theta) >= 0.75*m.pi:
+            #     if np.array(m.copysign(1,dtheta)) != np.array(m.copysign(1,action_space[a])):
+            #         action = np.array(m.copysign(1,-1))*action
+            # else:
+            #     if np.array(m.copysign(1,dtheta)) == np.array(m.copysign(1,action_space[a])):
+            #         action = np.array(m.copysign(1,-1))*action
+            
+            print(action)
+            observation, reward, done, info = env.step(action)
+                    
+    env.close()
 
 
 if __name__ == '__main__':
@@ -86,19 +165,21 @@ if __name__ == '__main__':
 
     # initialization variables
     LR = 1e-3 # learning rate
-    num_epochs = 5
-    goal_steps = 50 # how long each game runs
-    initial_games = 10 # how many games
+    num_epochs = 5 # number of times data is seen
+    games = 10 # how many games
+    steps = 50 # how long each game runs
     test_games = 5 # how test games
-    test_steps = 500 # how long each test game runs
-    # print(env.observation_space.shape[])
-    training_data = get_trainingData(initial_games,goal_steps)
+    test_steps = 50 # how long each test game runs
+    
+    training_data = getData(games,steps)
     np.save('saved_training_data0.npy',training_data)
 
-    options = {'lr':LR, 'num_epochs':num_epochs,'training_data':training_data}
-    # trainer = Trainer(options)
-    # trainer.train()
-    # for i, v in enumerate(training_data['training_inputs']):
-    #     print(v)
-    model = Model()
+    testing_data = getData(games,steps)
+    np.save('saved_testing_data0.npy',testing_data)
+
+    options = {'lr':LR, 'num_epochs':num_epochs,'training_data':training_data,'testing_data': testing_data}
+    trainer = Trainer(options)
+    trainer.train()
+    t = torch.from_numpy(testing_data['inputs']).float()
     
+